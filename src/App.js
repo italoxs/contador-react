2d37777699bdbfb667c58f591076adb18269125a
Italo Xavier Sales <italo.x.sales@hotmail.com>
2024-09-27 14:29:44 -0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import { useState } from "react";
 
+import './App.css'
+
 export default function App() {
   const [number, setNumber] = useState(0)
+  const [numberTwo, setNumberTwo] = useState(0)
 
-  function increase() {
+  function increase(contador) {
     setNumber(number + 1)
+    setNumberTwo(number + 1)
   }
 
   function decrease() {
     setNumber(number - 1)
+    setNumberTwo(number - 1)
   }
 
   return (
-    <section>
-      <h1>Contador</h1>
+    <>
+    <section id="contadorOne">
+      <h1>Contador 1</h1>
       <p>{number}</p>
-      <button type="button" onClick={decrease}>menos</button>
-      <button type="button" onClick={increase}>mais</button>
+      <div>
+        <button type="button" onClick={decrease}>-</button>
+        <button type="button" onClick={increase}>+</button>
+      </div>
     </section>
+</>
   );
 }
